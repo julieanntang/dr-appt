cc6741fa262e0c41ad562fbacb89799cbf9498cc
Julie Tang <julieanntang@hotmail.com>
2021-09-25 05:48:29 -0600

    cleaned up app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,21 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import { Container } from "semantic-ui-react";
 import { Route, Switch } from "react-router";
-import Home from "./pages/Home";
-import Appointments from "./pages/Appointments";
-import Patients from "./pages/Patients";
-import Physicians from "./pages/Physicians";
+import { routes } from "./components/routes";
 
 function App() {
+  const renderRoutes = () => {
+    return routes.map((route) => (
+      <Route exact path={route.pathname} component={route.component} />
+    ));
+  };
   return (
     <>
       <NavBar />
       <Container>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/appointments" component={Appointments} />
-          <Route exact path="/patients" component={Patients} />
-          <Route exact path="/physicians" component={Physicians} />
+          {renderRoutes()}
+          <Route component={() => <p>react router 404 path not found</p>} />
         </Switch>
       </Container>
     </>
